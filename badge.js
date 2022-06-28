456dbcf645d58ae4b0e57bca48283bd2d304a752
Ivan Bowen <66402923+874bowen@users.noreply.github.com>
2022-06-28 22:26:31 +0300

    some changes

--- badge.js
+++ badge.js
@@ -1,22 +1,17 @@
 window.onload = function(){
     var favicon = document.getElementById('favicon');
     var faviconSize = 16;
-
     var canvas = document.createElement('canvas');
-
     var context = canvas.getContext('2d');
     var img = document.createElement('img');
     img.src = favicon.href;
 
     img.onload = () => {
-
         canvas.width = faviconSize;
         canvas.height = faviconSize;
-
         // Draw background
         context.fillStyle = "#F76B67";
         context.fillRect(0, 0, canvas.width, canvas.height);
-
         // Draw text
         context.font = "10px 'helvetica', Assistant";
         context.fillStyle = "#FFFFFF";
